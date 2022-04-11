e437184cc53c753039f27ea4c19d466921bcb8d1
Vanessa <vanessa.randis@gmail.com>
2022-04-11 11:39:43 -0300

    CSharp POO Programa Banco

---
+++ ContaCorrente.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramBank
+{
+    public class ContaCorrente
+    {
+
+
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo = 100;
+
+        public bool Sacar(double valor)
+        {
+
+            if (this.saldo < valor)
+
+            {
+                Console.WriteLine("Infelizmente saldo insuficiente");
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                Console.WriteLine("Tranferencia realizada com sucesso");
+
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+
+
+        }
+
+        public bool transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+
+                Console.WriteLine("Saldo insuficiente ");
+
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.Depositar(valor);
+                Console.WriteLine("Tranferência realizada com sucesso");
+                return true;
+
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente GustavoConta = new ContaCorrente();
+            GustavoConta.titular = new Cliente();
+            GustavoConta.titular.nome = "Gustavo Veiga";
+
+           
+
+            Console.WriteLine("Insira o valor que gostaria de sacar");
+            GustavoConta.Sacar(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Saldo atual conta Gustavo : " + GustavoConta.saldo);
+
+            Console.WriteLine("Insira o valor que gostaria de depositar: ");
+            GustavoConta.Depositar(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Titular " + GustavoConta.titular.nome);
+
+            Console.WriteLine("Saldo atual conta Gustavo :" + GustavoConta.saldo);
+
+
+            
+
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = new Cliente();
+
+            contaDaGabriela.titular.nome="Gabriela Santos";
+
+            Console.WriteLine("Saldo conta Gustavo " + GustavoConta.saldo);
+            Console.WriteLine("Saldo conta da Gabriela " + contaDaGabriela.saldo);
+
+            GustavoConta.transferir(200.00, contaDaGabriela);
+
+            Console.WriteLine("Saldo atualizado das contas após transferência:");
+
+            Console.WriteLine("Saldo conta Gustavo " + GustavoConta.saldo);
+            Console.WriteLine("Saldo conta da Gabriela " + contaDaGabriela.saldo);
+
+            Console.WriteLine("Ao finalizar a execução tecle ENTER");
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
